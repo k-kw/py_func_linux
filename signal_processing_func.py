38aa398b7527a6d447d27f071c96d1882ecfd337
k-kw <kouki1212@chiba-u.jp>
2022-07-25 17:20:30 +0900

    signal process del comment

--- signal_processing_func.py
+++ signal_processing_func.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#return absolute
 def img_hpf(img,threshold):
     H=np.zeros(img.shape)
     for h in range(img.shape[0]):
@@ -14,19 +15,3 @@ def img_hpf(img,threshold):
     img=np.fft.ifft2(img)
     img=np.abs(img)
     return img
-
-
-
-# def img_read(fname):
-#  img = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
-#  return img
-# def img_write(fname, img):
-#  img = img.astype(np.uint8)
-#  cv2.imwrite(fname, img)
-#  return 0
-# img = img_read("mandrill.bmp")
-# img=np.fft.fft2(img)
-# img=np.fft.fftshift(img)
-# img=np.abs(img)
-# plt.imshow(np.log(img.real+1),"gray")
-#plt.show()
